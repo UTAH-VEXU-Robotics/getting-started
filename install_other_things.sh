cfe7790ceb6149a5ecb010e52ddb9e9557b2c6d1
potatehoes <acetousk@users.noreply.github.com>
2020-05-29 18:56:29 -0600

    Update install_other_things.sh

--- install_other_things.sh
+++ install_other_things.sh
@@ -3,7 +3,7 @@
 #installing other things
 sudo apt -y install ros-melodic-ros-tutorials
 sudo apt -y install ros-melodic-realsense2-camera
-sudo apt -y install ros-melodic-realsense2-description ros-melodic-joy
+sudo apt -y install ros-melodic-realsense2-description ros-melodic-joy ros-melodic-gazebo-ros
 sudo apt -y install snap snapd
 sudo apt -y install blender
 
