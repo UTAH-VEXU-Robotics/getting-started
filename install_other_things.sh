981bfdd8f4af134bc248a08b560a795abd519487
potatehoes <acetousk@users.noreply.github.com>
2020-06-07 23:10:57 -0600

    Update install_other_things.sh

--- install_other_things.sh
+++ install_other_things.sh
@@ -5,7 +5,7 @@ sudo apt -y install ros-melodic-ros-tutorials
 sudo apt -y install ros-melodic-realsense2-camera
 sudo apt -y install ros-melodic-realsense2-description ros-melodic-joy ros-melodic-gazebo-ros
 sudo apt -y install ros-melodic-rqt-common-plugins ros-melodic-rqt-robot-plugins
-sudo apt -y install ros-melodic-controller-manager ros-melodic-teleop-twist-joy ros-melodic-robot-state-publisher ros-melodic-message-to-tf ros-melodic-joint-state-controller
+sudo apt -y install ros-melodic-controller-manager ros-melodic-teleop-twist-joy ros-melodic-robot-state-publisher ros-melodic-message-to-tf ros-melodic-joint-state-controller ros-melodic-urdf-tutorial
 
 
 sudo apt -y install snap snapd
